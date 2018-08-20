212d66e8e1f553db177adc744715f88c56e81e5c
u188242 <u188242@L3F0Y3M2.bcbsnc.com>
2018-08-20 16:41:21 -0400

    crud functionality for brand_name table

--- snmroot/src/main/resources/static/snmrootController.js
+++ snmroot/src/main/resources/static/snmrootController.js
@@ -23,9 +23,12 @@ var app = angular.module('myApp', []);
 		$scope.showITlistSection = false;
 		$scope.showISTlistSection = false;
 		$scope.showBrandNamelistSection = false;
+		$scope.BrandNameFormId = null;
+		$scope.BrandNameFormDesc = null;
 		$scope.showITForm = false;
 		$scope.showISTform = false;
 		$scope.showBrandNameForm = false;
+		$scope.brandNameFormEdit = false;
 		$scope.ingredientTypesList = [];
 		$scope.ingredientSubtypeList = [];
 		$scope.brandNamesList = [];
@@ -211,6 +214,39 @@ var app = angular.module('myApp', []);
 				
 			});
 		}
+		$scope.getIngredientTypeList = function() {
+			var url = '/snmroot/ingredienttype/maintype/all';
+			$http.get(url).then(function(response) {
+				data = response.data;
+				if(typeof data!=="undefined") {
+					$scope.ingredientTypesList = data;
+					console.log('ingredient type length = ' , data.length);
+				}
+				console.log('Body:', data);
+			});
+		}
+		$scope.getIngredientSubtypeList = function() {
+			var url = '/snmroot/ingredienttype/subtype/all';
+			$http.get(url).then(function(response) {
+				data = response.data;
+				if(typeof data!=="undefined") {
+					$scope.ingredientSubtypeList = data;
+					console.log('ingredient subtype length = ' , data.length);
+				}
+				console.log('Body:', data);
+			});
+		}
+		$scope.getBrandNameList = function() {
+			var url = '/snmroot/ingredienttype/brand/all';
+			$http.get(url).then(function(response) {
+				data = response.data;
+				if(typeof data!="undefined") {
+					$scope.brandNamesList = data;
+					console.log('brands length = ' + data.length);
+				}
+				console.log('Body:', data);
+			});
+		}
 		$scope.showIT = function() {
 			$scope.showITlistSection = true;
 			$scope.showISTlistSection = false;
@@ -235,7 +271,97 @@ var app = angular.module('myApp', []);
 			$scope.showISTform = false;
 			$scope.showBrandNameForm = false;
 		}
-		$scope.updateBrand = function(id) {
-			console.log('brand id # = ' + id);
+		$scope.updateBrand = function(brand) {
+			$scope.showBrandNameForm = true;
+			if (brand == 'new') {
+				$scope.BrandNameFormId = null;
+				$scope.BrandNameFormDesc = null;
+				$scope.brandNameFormEdit = false;
+			} else {
+				console.log('brand id # = ' + brand.id);
+				$scope.BrandNameFormId = brand.id;
+				$scope.BrandNameFormDesc = brand.brand_name_desc;
+				$scope.brandNameFormEdit = true;
+			}
+			
+		}
+		$scope.deleteIngredientType = function(id) {
+			console.log('ingredient type id to be deleted = ' + id);
+			var url = '/snmroot/ingredienttype/maintype/delete/' + id;
+			$http.delete(url).then(function(response) {
+					status = response.status;
+					console.log('status = ', status);
+					$scope.getIngredientTypeList();
+				});
+		}
+		$scope.deleteIngredientSubtype = function(id) {
+			console.log('ingredient subtype id to be deleted = ' + id);
+			var url = '/snmroot/ingredienttype/subtype/delete/' + id;
+			$http.delete(url).then(function(response) {
+					status = response.status;
+					console.log('status = ', status);
+					$scope.getIngredientSubtypeList();
+				});
+		}
+		$scope.deleteBrand = function(id) {
+			console.log('brand id to be deleted = ' + id);
+			var url = '/snmroot/ingredienttype/brand/delete/' + id;
+			$http.delete(url).then(function(response) {
+					status = response.status;
+					console.log('status = ', status);
+					$scope.getBrandNameList();
+				});
+		}
+		$scope.saveBrandName = function() {
+				if($scope.BrandNameFormId !== null) {
+					$scope.updateBrandName();
+				} else {
+					$scope.insertBrandName();
+				}
+		}
+		$scope.insertBrandName = function() {
+			item = {
+					"brand_name_desc" : $scope.BrandNameFormDesc
+				};
+				var json = JSON.stringify(item);
+				var req = {
+					method : 'POST',
+					url : '/snmroot/ingredienttype/brand/insert',
+					headers : {
+						'Content-Type' : 'application/json'
+					},
+					data : json
+				};
+				$http(req).then(function(response) {
+					data = response.data;
+					$scope.showBrandNameForm = false;
+					$scope.BrandNameFormId = null;
+					$scope.BrandNameFormDesc = null;
+					$scope.brandNameFormEdit = false;
+					$scope.getBrandNameList();
+				});		
+		}
+		$scope.updateBrandName = function() {
+			item = {
+					"id" : $scope.BrandNameFormId,
+					"brand_name_desc" : $scope.BrandNameFormDesc
+				};
+				var json = JSON.stringify(item);
+				var req = {
+					method : 'POST',
+					url : '/snmroot/ingredienttype/brand/update',
+					headers : {
+						'Content-Type' : 'application/json'
+					},
+					data : json
+				};
+				$http(req).then(function(response) {
+					data = response.data;
+					$scope.showBrandNameForm = false;
+					$scope.BrandNameFormId = null;
+					$scope.BrandNameFormDesc = null;
+					$scope.brandNameFormEdit = false;	
+					$scope.getBrandNameList();
+				});	
 		}
 	});
